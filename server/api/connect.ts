c589f275693c6f4284320f51c09128aaaf6c2341
Michael Lebedew <michael.lebedew@web-dienstleister.de>
2023-05-26 10:00:15 +0200

    :green_heart: flush headers

--- server/api/connect.ts
+++ server/api/connect.ts
@@ -1,21 +1,23 @@
 import store from '../store'
 
 export default defineEventHandler(async (event) => {
-
+    const res = event.node.res;
     // Enable SSE endpoint
     setHeader(event, 'cache-control', 'no-cache')
     setHeader(event, 'connection', 'keep-alive')
     setHeader(event, 'content-type', 'text/event-stream')
     setResponseStatus(event, 204);
+    res.flushHeaders();
+
 
     let counter = 0
 
     const sendEvent = (data: any) => {
         console.log(`Event ${ counter } was sent`);
-        const res = event.node.res;
+
         res.write(`id: ${ ++counter }\n`);
         res.write(`data: ${ JSON.stringify(store.getVotes()) }\n\n`);
-        res.flushHeaders();
+
     }
 
     store.addListener(() => {
