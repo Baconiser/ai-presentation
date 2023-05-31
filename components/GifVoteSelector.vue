20978b7c61283c99acf18d67217436b32d2c0c28
Michael Lebedew <michael.lebedew@web-dienstleister.de>
2023-05-31 14:46:32 +0200

    :sparkles: :sparkles: :sparkles: :sparkles: :sparkles: :confetti:

--- components/GifVoteSelector.vue
+++ components/GifVoteSelector.vue
@@ -4,7 +4,7 @@ import IdUtil from '~/utils/IdUtil'
 import ParticleEffect from "~/components/ParticleEffect.vue"
 
 
-function voteForGif (gifId: string) {
+function voteForGif(gifId: string) {
   const userId = IdUtil.getId()
   fetch('/api/vote_gif/', {
     body: JSON.stringify({ userId, gifId }),
@@ -18,16 +18,29 @@ function voteForGif (gifId: string) {
 </script>
 <template>
   <div
-    class="slide overflow-hidden w-screen
+      class="slide overflow-hidden w-screen
       h-screen flex flex-col relative align-center"
   >
     <div class="w-full grow-1 flex flex-col overflow-hidden">
+      <div class="w-full relative">
+        <ParticleEffect/>
         <img class="w-full" :src="`/${GifIds.boo}.webp`" @click="voteForGif(GifIds.boo)">
+      </div>
+      <div class="w-full relative">
+        <ParticleEffect/>
         <img class="w-full" :src="`/${GifIds.mindblow}.webp`" @click="voteForGif(GifIds.mindblow)">
+      </div>
+
     </div>
     <div class="w-full flex flex-row overflow-hidden">
-      <img class="h-full max-w-1/2" :src="`/${GifIds.huh}.webp`" @click="voteForGif(GifIds.huh)">
-      <img class="h-full max-w-1/2" :src="`/${GifIds.nice}.webp`" @click="voteForGif(GifIds.nice)">
+      <div class="w-full">
+        <ParticleEffect/>
+        <img class="h-full max-w-1/2" :src="`/${GifIds.huh}.webp`" @click="voteForGif(GifIds.huh)">
+      </div>
+      <div class="w-full">
+        <ParticleEffect/>
+        <img class="h-full max-w-1/2" :src="`/${GifIds.nice}.webp`" @click="voteForGif(GifIds.nice)">
+      </div>
     </div>
   </div>
 </template>
