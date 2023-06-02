bd485077262cf35b7844d5624383c8e6cc8ab30b
Quirin Koch <quirin.koch@web-dienstleister.de>
2023-06-02 10:27:38 +0200

    :sparkles: Selbstgenerierte Bilder

--- components/slides/BoringHistorySlide.vue
+++ components/slides/BoringHistorySlide.vue
@@ -1,11 +1,12 @@
 <script lang="ts" setup>
+import abraham from '@/assets/images/boring/abraham.jpg'
 </script>
 
 <template>
   <BoringSlide
     title="Geschichte"
     :statements="['1800', 'erstes mal', 'mehr daten die auch einen langen text haben, der viel erklaert zu dem Thema']"
-    src="https://image.lexica.art/full_jpg/17fb5c1d-ef2b-4c55-836d-19ddc69ccf8b"
+    :src="abraham"
   />
 </template>
 
