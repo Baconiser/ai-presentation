909cfb8f256151c613245a79330e65c973b2a7b3
Quirin Koch <quirin.koch@web-dienstleister.de>
2023-06-22 14:07:32 +0200

    Umstrukturierung

--- components/slides/BoringNewsSlide.vue
+++ components/slides/BoringNewsSlide.vue
@@ -1,13 +1,24 @@
 <script lang="ts" setup>
+import { onKeyStroke } from '@vueuse/core'
 
 const steps = ref(-2)
-
+const showPointer = ref(false)
+onKeyStroke('q', () => {
+  showPointer.value = !showPointer.value
+})
 </script>
 
 <template>
   <boring-slide>
     <template #content>
-      <div class="grid grid-cols-3 items-center h-full relative w-full" @click="steps < 3 ? steps += 1 : steps === 3">
+      <div
+        :class="{'grid-cols-4': showPointer}"
+        class="grid grid-cols-3 items-center h-full relative w-full"
+        @click="steps < 3 ? steps += 1 : steps === 3"
+      >
+        <template v-if="showPointer">
+          <video src="@/assets/videos/jesus.mp4" loading="lazy" controls />
+        </template>
         <template v-if="steps < 3">
           <div class="w-full h-full flex items-center">
             <img v-show="steps > 0" src="@/assets/images/news/papst.jpg">
